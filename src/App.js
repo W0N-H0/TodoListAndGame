e6c5f67b98b7e42ca4e1976a30de5512e17a1809
WONHO22 <dnjsgh2122@gmail.com>
2023-07-02 00:41:52 +0900

    Design: imgs 폴더 및  배경이미지 추가

--- src/App.js
+++ src/App.js
@@ -3,10 +3,24 @@ import React, { useState, useEffect } from "react";
 import TodoList from "./TodoList";
 import styled from "styled-components";
 
+const BackgroundImage = styled.div`
+  background-image: url("./imgs/backgroundimage.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+`;
+
 function App() {
   return (
     <>
-      <TodoList></TodoList>
+      <BackgroundImage>
+        <div>테스트</div>
+        <TodoList></TodoList>
+      </BackgroundImage>
     </>
   );
 }
