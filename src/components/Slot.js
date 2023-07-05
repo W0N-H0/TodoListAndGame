2896ac935c67c9d150392620849a283ab74306fa
WONHO22 <dnjsgh2122@gmail.com>
2023-07-05 13:04:06 +0900

    Comment: Slot 컴포넌트 상세 주석 추가

--- src/components/Slot.js
+++ src/components/Slot.js
@@ -13,11 +13,14 @@ function Slots() {
   const [food3, setFood3] = useState(javascript);
   const [rolling, setRolling] = useState(false);
 
+  // 룰렛이 다돌아갔는지를 확인하는 상태
   const [isFinished, setisFinished] = useState(false);
   // 모달 on off를 위한 상태 추가
   const [openModal, setopenModal] = useState(true);
   console.log(openModal);
 
+  // DOM요소에 접근하고, 조작하기 위해서 useRef훅 사용
+  // 룰렛을 돌리기 위해서는 룰렛요소가 담긴 배열 컨테이너의 top 속성을 조작해줘야 하기 때문임
   const slotRefs = [useRef(null), useRef(null), useRef(null)];
 
   const foods = [
@@ -27,6 +30,9 @@ function Slots() {
   ];
 
   useEffect(() => {
+    // rolling이 끝난경우 useEffect 콜백함수 실행
+    // 이 룰렛은 slot1,2,3가 독립적으로 돌아감.
+    // 룰렛이 다 돌아간경우 slot1의 결과(top속성)을 slot2,3에도 동일하게 귀속시키기 위한 코드
     if (!rolling) {
       const slot1Top = slotRefs[0].current.style.top;
       const slot3Top = `calc(${slot1Top} - 1px)`;
@@ -38,13 +44,16 @@ function Slots() {
     }
   }, [rolling, isFinished]);
 
+  // 실제 click을 눌럿을때, 룰렛이 돌아가게하는 함수
   const roll = () => {
     const totalRotations = 10;
     setisFinished(!isFinished);
     setRolling(true);
+    // 0.5초 간격으로 setInterval 안에 있는 콜백함수를 반복적으로 실행
     const rotationInterval = setInterval(() => {
       slotRefs.forEach((slotRef, i) => {
         const selected = triggerSlotRotation(slotRef, i, foods);
+        console.log(selected);
         if (i === 0) {
           const foodIndex = foods[0].indexOf(selected);
           setFood1(selected);
@@ -65,11 +74,13 @@ function Slots() {
     }, totalRotations * 400);
   };
 
+  //
   const triggerSlotRotation = (slotRef, slotIndex, foods) => {
+    // dom요소의 top css속성을 변화시키기 위한 함수
     function setTop(top) {
       slotRef.current.style.top = `${top}px`;
     }
-
+    // 여기서 options는 슬롯 목록을 담고있는 container를 말함 ex) [javascript, typescript, reactf, vue, node]
     const options = slotRef.current.children;
     const filteredFoods = foods[slotIndex];
     const randomOption = Math.floor(Math.random() * filteredFoods.length);
