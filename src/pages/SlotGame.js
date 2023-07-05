bd3c2356ac82a055412b2b2e2818698da6793b84
WONHO22 <dnjsgh2122@gmail.com>
2023-07-05 13:40:53 +0900

    Feat: SlotGame 페이지 뒤로가기 버튼 구현

--- src/pages/SlotGame.js
+++ src/pages/SlotGame.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import Slot from "../components/Slot";
 import backgroundImg from "../img/background.jpg";
+import { useNavigate } from "react-router-dom";
 
 const Background = styled.div`
   background-image: url(${backgroundImg});
@@ -27,12 +28,39 @@ const Background = styled.div`
     padding: 5px 15px 5px 20px;
     border: 4px dashed white;
   }
+
+  > .goToMain {
+    position: fixed;
+    top: 0px;
+    left: 0px;
+    color: white;
+    border-radius: 5px;
+    border: 3px dashed white;
+    padding: 7px;
+    background: rgba(255, 88, 238, 0.95);
+    font-size: 1.5rem;
+    margin: 1px;
+    cursor: pointer;
+
+    &:hover {
+      background: #a466f7;
+    }
+  }
 `;
 
 function SlotGame() {
+  const navigate = useNavigate();
+
+  const handleGoToMain = () => {
+    navigate("/");
+  };
+
   return (
     <>
       <Background>
+        <div className="goToMain" onClick={handleGoToMain}>
+          GO BACK
+        </div>
         <div className="title">
           WELCOME TO <br /> CODING LAND!
         </div>
